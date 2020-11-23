15148a24501066fcace12ef016bf5978e0fc9d82
Vidur Gupta <vidurgupta01@gmail.com>
2020-11-23 04:20:41 -0800

    Readded File IO for Dongle Data

--- src/interfaces/remote/client/ws-client.js
+++ src/interfaces/remote/client/ws-client.js
@@ -4,6 +4,7 @@
 // connector for WebSockets in client mode
 
 import ws from "ws";
+import fs from "fs";
 import ReconnectingWebSocket from "reconnecting-websocket";
 
 import { Connector } from "../connector.js";
@@ -12,6 +13,7 @@ import msgRemote from "../msg-remote.js";
 import { CmdMsg, CmdCode } from "../../../comms/command.js";
 
 import logging from "../../../include/logging.js";
+import fsops from "../../../include/fs-ops.js";
 
 const TAG = "WsConnector";
 
@@ -47,6 +49,17 @@ export class WsClientConnector extends Connector {
     pause(time) {
         // We need to pause the dongle and invoke it after a set amount of time
         this.rws.close();
+
+        // Get endpoint dongle sending status
+        const file = fsops.getPrefixFile("curr_device.json", "data");
+        let devices_info = JSON.parse(fs.readFileSync(file, "utf8"));
+
+        // Reset limits
+        if (msg_json["params"]["pause_time"] == (60 * 60 * 24 * 1000)) {
+            devices_info["daily_msg"] = [];
+        }
+        devices_info["hourly_msg"] = [];
+        fs.writeFileSync(file, JSON.stringify(devices_info));
 
         // schedule reconnecting
         setTimeout(() => {
